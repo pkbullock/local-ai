9990f6614367bd2852468e833b0d1799a5868000
Paul Bullock <paul.bullock@capacreative.co.uk>
2024-07-27 08:28:03 +0100

    Sementic Kernel Sample based on Phi3 cookbook sample

---
+++ console-app/AI.LocalClient.SK.GettingStarted/Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+
+var modelPath = Path.Join(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),
+        @"\.aitk\models\microsoft\Phi-3-mini-4k-instruct-onnx\cpu_and_mobile\cpu-int4-rtn-block-32-acc-level-4");
+
+#pragma warning disable SKEXP0070 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+
+// create kernel
+var builder = Kernel.CreateBuilder();
+builder.AddOnnxRuntimeGenAIChatCompletion(modelId: "Phi-3-mini-4k-instruct-onnx", modelPath: modelPath);
+var kernel = builder.Build();
+
+// create chat
+var chat = kernel.GetRequiredService<IChatCompletionService>();
+var history = new ChatHistory();
+
+// run chat
+while (true)
+{
+    Console.Write("Q: ");
+    var userQ = Console.ReadLine();
+    if (string.IsNullOrEmpty(userQ))
+    {
+        break;
+    }
+    history.AddUserMessage(userQ);
+
+    Console.Write($"Phi3: ");
+    var response = "";
+    var result = chat.GetStreamingChatMessageContentsAsync(history);
+    await foreach (var message in result)
+    {
+        Console.Write(message.Content);
+        response += message.Content;
+    }
+    history.AddAssistantMessage(response);
+    Console.WriteLine("");
+}
